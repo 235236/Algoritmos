3b818db8d49dd7ddfdccccb81518fbb689353cc5
Alberto <Alberto@AlbertReal.gateway.2wire.net>
2015-05-13 11:07:00 -0600

    Analizador Léxico

--- src/tema1/Algoritmos.java
+++ src/tema1/Algoritmos.java
@@ -2,8 +2,7 @@ package tema1;
 
 /**
  *
- * @author David Alberto Torres Real 235236
- * Tema 1 de la tarea 52 Algoritmos
+ * @author David Alberto Torres Real 235236 Tema 1 de la tarea 52 Algoritmos
  * @version 1.0
  */
 public class Algoritmos {
@@ -29,9 +28,27 @@ public class Algoritmos {
         /**
          * Imprime los numeros pares entre 0 y 100.
          */
-        for (int i = 0; i <= 100; i++) {
+        for (int i = 0; i <= 100; i = i + 2) {
             System.out.println(i);
         }
+
+        /**
+         * Imprime la suma de los 100 primeros numeros.
+         */
+        int suma = 0;
+        for (int i = 0; i <= 100; i++) {
+            suma = suma + i;
+        }
+        System.out.println(suma);
+        
+        /**
+         * Imprime los numeros impares hasta el 100 y dice cuantos son.
+         */
+        //int suma = 0;
+        for (int i = 0; i <= 100; i++) {
+            suma = suma + i;
+        }
+        System.out.println(suma);
     }
 
 }
